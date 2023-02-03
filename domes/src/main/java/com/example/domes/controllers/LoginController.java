d1abf1abe4529955e8eb68ffd547edf8fa22807c
TomBascou <bascoutom.pro@gmail.com>
2023-02-03 10:39:58 +0100

    Interface
    wsh

--- domes/src/main/java/com/example/domes/controllers/LoginController.java
+++ domes/src/main/java/com/example/domes/controllers/LoginController.java
@@ -2,24 +2,18 @@ package com.example.domes.controllers;
 
 import com.example.domes.beans.Client;
 import com.example.domes.beans.User;
-import com.example.domes.repository.UserRepository;
-import com.example.domes.service.LoginService;
+import com.example.domes.service.ServiceLogin;
+import com.example.domes.service.ServiceLoginImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.sound.midi.SysexMessage;
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class LoginController {
     @Autowired
-    private LoginService loginService;
+    private ServiceLogin loginService;
 
     @PostMapping ("/loginUser")
     public String loginUser(@ModelAttribute("newuser") User user, Model model){
