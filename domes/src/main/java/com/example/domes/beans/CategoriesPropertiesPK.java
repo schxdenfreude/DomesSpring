9e91d656a932ea8a20abfeccdd5efee58e8c695a
LfvrYanis <lefevreyanis@outlook.fr>
2023-02-12 14:59:04 +0100

    Update CategoriesPropertiesPK.java

--- domes/src/main/java/com/example/domes/beans/CategoriesPropertiesPK.java
+++ domes/src/main/java/com/example/domes/beans/CategoriesPropertiesPK.java
@@ -2,6 +2,7 @@ package com.example.domes.beans;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 
@@ -12,4 +13,17 @@ public class CategoriesPropertiesPK implements Serializable {
 
     @ManyToOne
     private Properties properties;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CategoriesPropertiesPK that = (CategoriesPropertiesPK) o;
+        return Objects.equals(category, that.category) &&
+                Objects.equals(properties, that.properties);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(category, properties);
+    }
 }
